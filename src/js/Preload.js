90ee3161914509ec3e257aeeb340f82ddd28b72b
Timo Reitz <godsboss@gmx.de>
2016-04-16 09:00:19 +0200

    Add shape gfx.

--- src/js/Preload.js
+++ src/js/Preload.js
@@ -6,9 +6,13 @@ class Preload {
     this.load.setPreloadSprite(preloaderBar);
 
     [
+      'hexagon',
       'intro-background',
       'level-playable',
-      'level-select-marker'
+      'level-select-marker',
+      'pentagon',
+      'square',
+      'triangle'
     ].forEach((key) => this.load.image(key, 'gfx/' + key + '.png'));
 
     this.load.json('level-data', 'levels.json');
