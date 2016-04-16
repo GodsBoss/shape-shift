e7329921b3088bec7f29fe2ef0e41dad919fbdaf
Timo Reitz <godsboss@gmx.de>
2016-04-16 03:13:58 +0200

    Minimal build process: Create index.html

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,12 @@ gulp.task(
   'default',
   function() {}
 );
+
+gulp.task(
+  'build:index.html',
+  function() {
+    gulp.
+      src('src/html/index.html').
+      pipe(gulp.dest('dist'));
+  }
+);
