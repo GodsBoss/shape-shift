ae4e22cd0a171b6ba6093a5740ca784b61f3e056
Timo Reitz <godsboss@gmx.de>
2016-04-17 00:52:34 +0200

    Let turn specials rotate.

--- src/js/Turn.js
+++ src/js/Turn.js
@@ -2,6 +2,7 @@ class Turn extends Phaser.Sprite {
   constructor(game, x, y, key, frame) {
     super(game, x, y, key, frame);
     this.anchor.setTo(0.5, 0.5);
+    this.rotation = Math.random() * Math.PI * 2;
   }
 
   turn(shape) {
@@ -10,4 +11,11 @@ class Turn extends Phaser.Sprite {
       y: (this.direction === 'right' ? 1 : -1) * shape.velocity.x
     };
   }
+
+  update() {
+    super.update();
+    this.rotation += (this.direction === 'right' ? 1 : -1) * Turn.rotationSpeed;
+  }
 }
+
+Turn.rotationSpeed = 0.02;
